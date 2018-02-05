df0cb70be86fc3f429a98ff0a14ffe027dc91675
Max Soe <maxsoe@gmail.com>
2018-02-05 00:28:17 +0000

    Lesson 10 - Phone details template

--- phone-detail/phone-detail.component.js
+++ phone-detail/phone-detail.component.js
@@ -2,18 +2,19 @@
 angular
   .module('phoneDetail', ['ui.router', 'getPhoneDetailServ'])
   .component('phoneDetail', {
-    template: 'TBD: Detail view for <span>{{phoneDetailCtrl.phoneId}}</span>',
+    // template: 'TBD: Detail view for <span>{{phoneDetailCtrl.phoneId}}</span>',
+    templateUrl: 'phone-detail/phone-detail.template.html',
     controller: PhoneDetailController,
     controllerAs: 'phoneDetailCtrl'
   });
 
 function PhoneDetailController($stateParams, getPhoneDetailServ) {
   this.phoneId = $stateParams.phoneId;
-  
+
   console.log("phoneId is " + this.phoneId);
 
   getPhoneDetailServ.getPosts(this.phoneId).then(response => {
-    this.detail = response.data;
-    console.log(this.detail);
+    this.phone = response.data;
+    console.log(this.phone);
   });
 };
