64d90543321f23c94f637227565bd384aae5ee22
Max Soe <maxsoe@gmail.com>
2018-02-05 00:13:35 +0000

    Phone detail service

---
+++ core/get-phone-detail.service.js
@@ -0,0 +1,10 @@
+angular
+  .module('getPhoneDetailServ', [])
+  .service('getPhoneDetailServ', function($http) {
+    this.getPosts = function(parameter) {
+      console.log("the parameter is " + parameter);
+      var url = 'phones/' + parameter + '.json';
+      console.log("the url is " + url);
+      return $http.get(url);
+    }
+  });
--- phone-detail/phone-detail.component.js
+++ phone-detail/phone-detail.component.js
@@ -1,12 +1,19 @@
 // Register `phoneDetail` component, along with its associated controller and template
 angular
-  .module('phoneDetail', ['ui.router'])
+  .module('phoneDetail', ['ui.router', 'getPhoneDetailServ'])
   .component('phoneDetail', {
     template: 'TBD: Detail view for <span>{{phoneDetailCtrl.phoneId}}</span>',
     controller: PhoneDetailController,
     controllerAs: 'phoneDetailCtrl'
   });
 
-  function PhoneDetailController($stateParams) {
-    this.phoneId = $stateParams.phoneId;
-  };
+function PhoneDetailController($stateParams, getPhoneDetailServ) {
+  this.phoneId = $stateParams.phoneId;
+  
+  console.log("phoneId is " + this.phoneId);
+
+  getPhoneDetailServ.getPosts(this.phoneId).then(response => {
+    this.detail = response.data;
+    console.log(this.detail);
+  });
+};
