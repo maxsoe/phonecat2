799b7602205a5a9df12f35a7fbf9829af800487b
Max Soe <maxsoe@gmail.com>
2018-02-06 11:45:17 +0000

    Comment out some console log flags

--- core/get-phone-detail.service.js
+++ core/get-phone-detail.service.js
@@ -2,9 +2,9 @@ angular
   .module('getPhoneDetailServ', [])
   .service('getPhoneDetailServ', function($http) {
     this.getPosts = function(parameter) {
-      console.log("the parameter is " + parameter);
+      // console.log("the parameter is " + parameter);
       var url = 'phones/' + parameter + '.json';
-      console.log("the url is " + url);
+      // console.log("the url is " + url);
       return $http.get(url);
     }
   });
